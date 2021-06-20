30017d04be13b7c89638aae5c3db7f5907f56416
Hyunwoo Nam <namenu@gmail.com>
2021-06-20 17:46:29 +0900

    poc

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "2.3.1"]
+                 [clj-http "3.12.2"]
                  [clojure.java-time "0.3.2"]
                  [clojusc/protobuf "3.5.1-v1.1"]
                  [com.google.protobuf/protobuf-java "3.17.3"]])
--- src/clojure/core.clj
+++ src/clojure/core.clj
@@ -2,40 +2,78 @@
   (:require [protobuf.core :as pb]
             [clojure.java.io :as io]
             [clojure.data.json :as json]
+            [clj-http.client :as http]
             [java-time :as time]
             [java-time.format :as tf])
-  (:import (mdg.engine.proto Reports$Field Reports$ReportHeader)))
+  (:import (mdg.engine.proto Reports$ReportHeader Reports$FullTracesReport Reports$Traces Reports$Trace Reports$TracesReport)))
 
-(pb/create Reports$ReportHeader
-           {:hostname   "api-clj.farmmorning.com"
-            :schemaTag  ""
-            :schemaHash ""}) )
+(def endpoint "https://usage-reporting.api.apollographql.com/api/ingress/traces")
 
-
-(let [start-time   (time/zoned-date-time s)
-      start-offset (time/nanos 1000000000)]
-  (time/plus start-time start-offset))
+(def X-Api-Key "")
 
 (defn ->Trace [{:keys [path parentType fieldName returnType startOffset duration]}]
-  (let [startOffset
-        parentType (:parentType trace)
-        ]
-    (pb/create Reports$Trace
-               {:endTime ""
-                :startTime})))
+  #_(let [startTime  (+ startTime startOffset)
+          parentType (:parentType trace)
+          root       nil])
+  (pb/create Reports$Trace
+             {:start_time  ""
+              :end_time    ""
+              :duration_ns duration}))
 
-(-> (pb/create Reports$Field
-               {:name        "Alice"
-                :return_type "ret"})
-    )
+(defn aggregate-tracing [tracing]
+  (let [t1     (->Trace {:duration 1000000})
+        traces (pb/create Reports$Traces
+                          {:trace [t1]})]
+    [{:key   "q1"
+      :value traces}]))
 
-(def sample
-  (json/read-str (slurp (io/resource "sample.json"))))
+(defn ->TracesReport [tracing]
+  (let [header (pb/create Reports$ReportHeader
+                          {})
+        t1     (->Trace {:duration 1000000})]
+    (pb/create Reports$TracesReport
+               {:header header
+                :trace  [t1]})))
 
-(def tracing
-  (read-string (slurp (io/resource "lacinia-tracing.edn"))))
+(defn ->FullTracesReport [tracing]
+  (let [header           (pb/create Reports$ReportHeader
+                                    {:hostname   "api-clj.farmmorning.com"
+                                     :schemaTag  ""
+                                     :schemaHash ""})
+        traces_per_query (aggregate-tracing tracing)]
+    (pb/create Reports$FullTracesReport
+               {:header           header
+                :traces_per_query traces_per_query})))
 
-(def trace
-  (first (get-in tracing [:tracing :execution :resolvers])))
 
-(->Trace trace)
+(defn send-report [tracing]
+  (let [report (->TracesReport tracing)]
+    (try
+      (http/post endpoint
+                 {:headers      {"X-Api-Key" X-Api-Key}
+                  :content-type :application/protobuf
+                  :body         (io/input-stream
+                                  (pb/->bytes report))})
+      (catch Exception e
+        (ex-data e)))))
+
+
+(comment
+
+  (def sample
+    (json/read-str (slurp (io/resource "sample.json"))))
+
+  (def tracing
+    (read-string (slurp (io/resource "lacinia-tracing.edn"))))
+
+
+  (let [start-time   (time/zoned-date-time s)
+        start-offset (time/nanos 1000000000)]
+    (time/plus start-time start-offset))
+
+  (aggregate-tracing tracing)
+  (->FullTracesReport tracing)
+
+  (send-report tracing)
+
+  )
